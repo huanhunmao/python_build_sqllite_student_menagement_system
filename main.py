22a33dac9a39ccdc630852b550bf8d13631b85a0
Marxu <fuhaojun@52tt.com>
2024-07-06 17:05:00 +0800

    feat: 使用 sql db 数据 导入

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QComboBox, QLabel, \
-    QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow,QTableWidget
+    QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, \
+    QTableWidgetItem
 
 from PyQt6.QtGui import QAction
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -24,11 +26,27 @@ class MainWindow(QMainWindow):
         # Create table
         self.table = QTableWidget()
         self.table.setColumnCount(4)
-        self.table.setHorizontalHeaderLabels(('id', 'Name', 'Course', 'Mobile'))
+        self.table.setHorizontalHeaderLabels(
+            ('id', 'Name', 'Course', 'Mobile'))
+        # 去掉第一列 id 前的 index
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+
+    def load_data(self):
+        connection = sqlite3.connect('database.db')
+        result = connection.execute("SELECT * FROM students")
+        # 确保 不重复
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number,
+                                   QTableWidgetItem(str(data)))
+        connection.close()
 
 
 app = QApplication(sys.argv)
 app_calculator = MainWindow()
+app_calculator.load_data()
 app_calculator.show()
 sys.exit(app.exec())
